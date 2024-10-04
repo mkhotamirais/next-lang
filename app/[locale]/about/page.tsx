f5035fac6c68279aadbb6a20270747d2ac49ff2a
Mkhotamirais <mkhotamirais@gmail.com>
2024-10-04 17:05:15 +0700

    berhasil kah

--- app/[locale]/about/page.tsx
+++ app/[locale]/about/page.tsx
@@ -1,3 +1,7 @@
-export default function AboutPage() {
+import { unstable_setRequestLocale } from "next-intl/server";
+
+export default function AboutPage({ params: { locale } }: { params: { locale: string } }) {
+  unstable_setRequestLocale(locale);
+
   return <div>AboutPage</div>;
 }
--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,7 +1,18 @@
 import { useTranslations } from "next-intl";
 import { Link } from "@/i18n/routing";
+import { unstable_setRequestLocale } from "next-intl/server";
 
-export default function HomePage() {
+// export async function generateMetadata({ params: { locale } }: { params: { locale: string } }) {
+//   const t = await getTranslations({ locale, namespace: "Metadata" });
+
+//   return {
+//     title: t("title"),
+//   };
+// }
+
+export default function HomePage({ params: { locale } }: { params: { locale: string } }) {
+  unstable_setRequestLocale(locale);
+
   const t = useTranslations("HomePage");
   return (
     <div>
